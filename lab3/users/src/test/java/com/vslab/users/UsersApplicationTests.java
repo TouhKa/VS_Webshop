38cdfe09edc2fd4f77a9f10fc7d4d43bd1ae1295
Daniel <danielkispert+github@googlemail.com>
2020-11-27 12:17:49 +0100

    users

---
+++ lab3/users/src/test/java/com/vslab/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vslab.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

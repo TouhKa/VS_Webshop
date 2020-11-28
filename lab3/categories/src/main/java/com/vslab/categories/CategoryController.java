e552ccbd75da030c2cbd5772db36dffd60cb886f
Pia Schmidt <pia.s.schmidt@gmail.com>
2020-11-28 15:59:48 +0100

    Add methods for core service "Category"

---
+++ lab3/categories/src/main/java/com/vslab/categories/CategoryController.java
@@ -0,0 +1,35 @@
+package com.vslab.categories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/category/")
+    public Category[] getAllCategories() {
+        return categoryService.getAll();
+    }
+
+    @PostMapping(value = "/category/", consumes = "application/json")
+    public Category addCategory(@RequestBody Category category){
+        return categoryService.addCategory(category);
+    }
+
+    @GetMapping("/category/{id}")
+    public Category getCategory(@PathVariable int id){
+        return categoryService.getCategoryById(id);
+    }
+
+    @PutMapping(value = "/category/", consumes = "application/json")
+    public Category updateCategory(@RequestBody Category category){
+        return categoryService.updateCategorie(category);
+    }
+    @DeleteMapping("/category/{id}")
+    public void deleteCategory(@PathVariable int id){
+        categoryService.deleteCategory(id);
+    }
+}

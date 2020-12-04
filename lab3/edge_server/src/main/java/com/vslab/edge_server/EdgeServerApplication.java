6d88d1d4365d69df54d7c804c384399ab6ec2592
Pia Schmidt <pia.s.schmidt@gmail.com>
2020-12-04 13:39:15 +0100

    Add config for routing all core and composite services Some Cleanup

--- lab3/edge_server/src/main/java/com/vslab/edge_server/EdgeServerApplication.java
+++ lab3/edge_server/src/main/java/com/vslab/edge_server/EdgeServerApplication.java
@@ -2,10 +2,13 @@ package com.vslab.edge_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class EdgeServerApplication {
 
 	public static void main(String[] args) {

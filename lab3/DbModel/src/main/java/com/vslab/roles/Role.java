d45ed983f146ef8282dc82bb3de086c8da48df9d
Daniel <danielkispert+github@googlemail.com>
2020-12-09 02:01:43 +0100

    added dbmodel project to hold the db objects

---
+++ lab3/DbModel/src/main/java/com/vslab/roles/Role.java
@@ -0,0 +1,28 @@
+package com.vslab.roles;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String type;
+
+    public Role(int id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
---
+++ lab3/DbModel/src/main/java/com/vslab/users/User.java
@@ -0,0 +1,57 @@
+package com.vslab.users;
+
+import com.vslab.roles.Role;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private Role role;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String username;
+
+    private String password;
+
+    public User(Role role, String firstname, String lastname, String username, String password) {
+        this.role = role;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}

b6fdf92d59ffa835ab1cb67c1ce54561da6a1703
Daniel <danielkispert+github@googlemail.com>
2021-01-26 14:23:17 +0100

    updated mvn

--- lab4/auth-server/src/main/java/com/vslab/authserver/Main.java
+++ lab4/auth-server/src/main/java/com/vslab/authserver/Main.java
@@ -1,4 +1,4 @@
-package com.vslab.authenticationservice;
+package com.vslab.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- lab4/auth-server/src/main/java/com/vslab/authserver/config/WebSecurityConfiguration.java
+++ lab4/auth-server/src/main/java/com/vslab/authserver/config/WebSecurityConfiguration.java
@@ -1,22 +1,21 @@
-package com.vslab.authenticationservice;
+package com.vslab.authserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 
-@Configuration
-@SuppressWarnings("deprecation")
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
     @Autowired
     private PasswordEncoder passwordEncoder;
 
@@ -32,17 +31,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-        UserDetails user = User.withUsername("admin")
-                                .password(passwordEncoder.encode("admin"))
-                                .authorities("read", "write")
-                                .build();
-        userDetailsManager.createUser(user); return userDetailsManager;
+    public UserDetailsService users() throws Exception {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("oauthuser").password("oauthpassword").roles("USER").build());
+        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+        return manager;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin(); http.authorizeRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }

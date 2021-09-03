5698187e8cbd0fc4bf00f056b58e67cbf0f24515
Daniel <danielkispert+github@googlemail.com>
2021-09-04 00:29:07 +0200

    fixed category

--- lab4/categories/src/main/java/com/vslab/webshop/CategoryService.java
+++ lab4/categories/src/main/java/com/vslab/webshop/CategoryService.java
@@ -1,13 +1,13 @@
-package com.vslab.categories;
+package com.vslab.webshop;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CategoryService {
+    
     @Autowired
     private CategoryRepo categoryRepo;
 
--- lab4/categories/src/test/java/com/vslab/webshop/CategoryRepositoryTest.java
+++ lab4/categories/src/test/java/com/vslab/webshop/CategoryRepositoryTest.java
@@ -1,4 +1,4 @@
-package com.vslab.categories;
+package com.vslab.webshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;

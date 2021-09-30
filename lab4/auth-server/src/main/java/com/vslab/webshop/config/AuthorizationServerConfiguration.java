3c3b0c2c1d3a0557a89fd334dc90a37b1394d476
TouhKa <pia.s.schmidt@googlemail.com>
2021-09-30 09:39:12 +0200

    Defining token access per service

--- lab4/auth-server/src/main/java/com/vslab/webshop/config/AuthorizationServerConfiguration.java
+++ lab4/auth-server/src/main/java/com/vslab/webshop/config/AuthorizationServerConfiguration.java
@@ -46,10 +46,16 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 //                .scopes("user_info", "role_info", "product_info", "category_info")
 //                .autoApprove(true)
 //                .and()
-                .withClient("webshop-client")
-                .secret(passwordEncoder.encode("webshop-secret"))
+                .withClient("user-service-client")
+                .secret(passwordEncoder.encode("user-service-secret"))
                 .authorizedGrantTypes("client_credentials")
                 .scopes("user_info")
+                .autoApprove(true)
+                .and()
+                .withClient("role-service-client")
+                .secret(passwordEncoder.encode("role-service-secret"))
+                .authorizedGrantTypes("client_credentials")
+                .scopes("role_info")
                 .autoApprove(true);
     }
 

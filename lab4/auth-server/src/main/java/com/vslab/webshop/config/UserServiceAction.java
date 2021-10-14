5217f9ae8c60e73882d57ae7855b9aafc66e8e80
Pia Schmidt <pia.s.schmidt@googlemail.com>
2021-10-14 19:39:55 +0200

    Login action (#22)

---
+++ lab4/auth-server/src/main/java/com/vslab/webshop/config/UserServiceAction.java
@@ -0,0 +1,63 @@
+package com.vslab.webshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.common.AuthenticationScheme;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableOAuth2Client
+@SuppressWarnings("deprecation")
+public class UserServiceAction {
+    private String authServerTokenURL = "http://auth-server:8092/oauth/token";
+    private String userServiceURL = "http://zuul:8091/users/";
+    private String clientId = "user-service-client";
+    private String clientSecret = "user-service-secret";
+    private String userServiceScope = "user_info";
+
+
+    //pass client credentials to corresponding service
+    public OAuth2ProtectedResourceDetails oAuth2ResourceDetails() {
+        ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
+        details.setClientId(clientId);
+        details.setClientSecret(clientSecret);
+        details.setAccessTokenUri(authServerTokenURL);
+
+        List<String> scope = new ArrayList<>();
+        scope.add(userServiceScope);
+        details.setScope(scope);
+
+        details.setAuthenticationScheme(AuthenticationScheme.header);
+        details.setClientAuthenticationScheme(AuthenticationScheme.header);
+        details.setId("1"); //is this needed?
+        details.setTokenName("User_Service");
+        return details;
+    }
+
+    @Bean(name = "restTemplate") // has to be done at runtime because the authorization server would not be up otherwise
+    public OAuth2RestTemplate awesomeRestTemplate() {
+        AccessTokenRequest atr = new DefaultAccessTokenRequest();
+        return new OAuth2RestTemplate(oAuth2ResourceDetails(), new DefaultOAuth2ClientContext(atr));
+    }
+
+    public User getUserByName(String username) {
+        OAuth2RestTemplate restTemplate = awesomeRestTemplate();
+
+            User[] users = restTemplate.getForObject(userServiceURL, User[].class);
+            for (int i = 0; i < users.length; i++) {
+                if (users[i].getUsername() == username) {
+                    return users[i];
+                }
+            }
+        return null;
+    }
+}
